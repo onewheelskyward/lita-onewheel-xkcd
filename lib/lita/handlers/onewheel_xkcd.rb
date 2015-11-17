73bd28110cdb27a4f51d73b988bbd3812009ad68
Andrew Kreps <andrew.kreps@gmail.com>
2015-11-17 11:29:42 -0800

    parameterized db config

--- lib/lita/handlers/onewheel_xkcd.rb
+++ lib/lita/handlers/onewheel_xkcd.rb
@@ -5,8 +5,11 @@ require_relative '../models/comic'
 module Lita
   module Handlers
     class OnewheelXkcd < Handler
-      # insert handler code here
-      DB = Sequel.connect('postgres://root@localhost:5432/lita_xkcd')
+      config :db_host, required: true
+      config :db_name, required: true
+      config :db_user, required: true
+      config :db_pass, required: true
+      config :db_port, required: true
 
       route /^xkcd$/i,
             :random,
@@ -19,15 +22,17 @@ module Lita
             help: {'xkcd (keyword)' => 'return an XKCD comic with the keyword specified.'}
 
       def find_by_keyword(response)
+        db = init_db
         keywords = response.matches[0][0]
         puts "select id, data->'img' as img, data->'title' as title, data->'alt' as alt from comics where data->>'title' ilike '%#{keywords}%' order by RANDOM() limit 1"
-        row = DB["select id, data->'img' as img, data->'title' as title, data->'alt' as alt from comics where data->>'title' ilike '%#{keywords}%' order by RANDOM() limit 1"][:data]
+        row = db["select id, data->'img' as img, data->'title' as title, data->'alt' as alt from comics where data->>'title' ilike '%#{keywords}%' order by RANDOM() limit 1"][:data]
         comic = Comic.new(row[:id], row[:img], row[:title], row[:alt])
         reply_with_comic response, comic
       end
 
       def random(response)
-        row = DB["select id, data->'img' as img, data->'title' as title, data->'alt' as alt from comics order by RANDOM() limit 1"][:data]
+        db = init_db
+        row = db["select id, data->'img' as img, data->'title' as title, data->'alt' as alt from comics order by RANDOM() limit 1"][:data]
         comic = Comic.new(row[:id], row[:img], row[:title], row[:alt])
         reply_with_comic response, comic
       end
@@ -35,6 +40,10 @@ module Lita
       def reply_with_comic(response, comic)
         response.reply "\"#{comic.title}\" #{comic.image}"
         # set timer for alt response
+      end
+
+      def init_db
+        Sequel.connect("postgres://#{config.db_user}:#{config.db_pass}@#{config.db_host}:#{config.db_port}/#{config.db_name}")
       end
       Lita.register_handler(self)
     end

dd50228e55f6a832d659cb522e4abd55c8537b39
Andrew Kreps <andrew.kreps@gmail.com>
2015-11-16 22:26:27 -0800

    added baseline keyword search

--- lib/lita/handlers/onewheel_xkcd.rb
+++ lib/lita/handlers/onewheel_xkcd.rb
@@ -9,25 +9,33 @@ module Lita
       # insert handler code here
       DB = Sequel.connect('postgres://root@localhost:5432/lita_xkcd')
 
-      route /xkcd\s*(random)?/i,
+      route /^xkcd$/i,
             :random,
             command: true,
-            help: {'xkcd random' => 'return a random XKCD comic.'}
+            help: {'xkcd' => 'return a random XKCD comic.'}
 
-      route /xkcd\s*(random)?/i,
-            :random,
+      route /xkcd (\w+)/i,
+            :find_by_keyword,
             command: true,
-            help: {'xkcd random' => 'return a random XKCD comic.'}
+            help: {'xkcd (keyword)' => 'return an XKCD comic with the keyword specified.'}
 
       def find_by_keyword(response)
         keywords = response.matches[0][0]
-        response.reply "keywords #{keywords}"
+        puts "select id, data->'img' as img, data->'title' as title, data->'alt' as alt from comics where data->>'title' ilike '%#{keywords}%' order by RANDOM() limit 1"
+        row = DB["select id, data->'img' as img, data->'title' as title, data->'alt' as alt from comics where data->>'title' ilike '%#{keywords}%' order by RANDOM() limit 1"][:data]
+        comic = Comic.new(row[:id], row[:img], row[:title], row[:alt])
+        reply_with_comic response, comic
       end
 
       def random(response)
         row = DB["select id, data->'img' as img, data->'title' as title, data->'alt' as alt from comics order by RANDOM() limit 1"][:data]
         comic = Comic.new(row[:id], row[:img], row[:title], row[:alt])
+        reply_with_comic response, comic
+      end
+
+      def reply_with_comic(response, comic)
         response.reply "\"#{comic.title}\" #{comic.image}"
+        # set timer for alt response
       end
       Lita.register_handler(self)
     end
